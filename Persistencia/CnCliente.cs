359bea533b8a89f8b1299482b165b996d072d5fb
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2022-11-07 12:57:06 -0600

    prueba

--- Persistencia/CnCliente.cs
+++ Persistencia/CnCliente.cs
@@ -8,22 +8,7 @@ namespace Persistencia
 
         CnCliente()
         {
-            string datos = "server=localhost;user=root;database=escuela;port=3306;password=root";
-            try
-            {
-                con = new MySqlConnection(datos);
-                con.Open();
-                MessageBox.Show("Coneccion exitosa o establecida");
-            }
-            catch(MySqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-
-            }
-            finally
-            {
-                con.Close();
-            }
+            //prueba
             
 
 
