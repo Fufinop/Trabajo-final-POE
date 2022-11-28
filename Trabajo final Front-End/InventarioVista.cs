8a0ea9af56124c83d5577a04488062e9c575f8c5
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2022-11-27 20:30:58 -0600

    formulario de inventario agregado

--- Trabajo final Front-End/InventarioVista.cs
+++ Trabajo final Front-End/InventarioVista.cs
@@ -1,109 +1,230 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Controladores;
+using Entidad;
+using Modelos;
+using MySql.Data.MySqlClient;
+using Negocio;
+using System.Drawing.Imaging;
 
 namespace Trabajo_final_Front_End
 {
+
     public partial class InventarioVista : Form
     {
+        CnEmpleado cnEmpleado = new CnEmpleado();
         public InventarioVista()
         {
             InitializeComponent();
+
+            cbxEstatus.Text = "Activo";
+            
+        }
+        private void cargarDatos()
+        {
+            dgvEmpleado.DataSource = cnEmpleado.obtenerDatos().Tables["tb1"];
+            DataGridViewImageColumn column = (DataGridViewImageColumn)dgvEmpleado.Columns[5];
+            column.ImageLayout = DataGridViewImageCellLayout.Zoom;
+        }
+        private void Modulo10_Load(object sender, EventArgs e)
+        {
+            cargarDatos();
+            tabControl1.TabPages.Remove(tabPage1);
+            pictureBox1 = null;
+            btnEditar.Enabled = false;
+        }
+        private void limpiarForm()
+        {
+            nudEmpleado.Value = 0;
+            tbxNombre.Text = string.Empty;
+            tbxApellidos.Text = string.Empty;
+            tbxSalario.Text = string.Empty;
+            cbxTipo.Text = string.Empty;
+            cbxEstatus.Text = "Activo";
+            //pictureBox1 = null;
         }
 
-        private void label5_Click(object sender, EventArgs e)
+        private void btnGuardar_Click_1(object sender, EventArgs e)
         {
 
-        }
-
-        private void dgvReceta_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void BtnActualizar_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnCancelar_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-        }
-
-        private void btnEliminar_Click(object sender, EventArgs e)
-        {
-            try
+            bool Resultado;
+            CeEmpleado ceEmpleado = new CeEmpleado();
+            ceEmpleado.Id = (int)nudEmpleado.Value;
+            ceEmpleado.Nombre = tbxNombre.Text;
+            ceEmpleado.Apellidos = tbxApellidos.Text;
+            ceEmpleado.Salario = tbxSalario.Text;
+            ceEmpleado.Tipo = cbxTipo.Text;
+            ceEmpleado.Estatus = cbxEstatus.Text;
+            if (pictureBox2.Image != null)
             {
-                dgvInventario.Rows.Remove(dgvInventario.CurrentRow);
+                ceEmpleado.Imagen = Imagen.imgToByte(pictureBox2.Image);
             }
-            catch { }
-        }
 
-        private void btnLimpiar_Click(object sender, EventArgs e)
-        {
-            dgvInventario.Rows.Clear();
-        }
 
-        private void btnNuevo_Click(object sender, EventArgs e)
-        {
-            if (nudInventario.Value == 0)
+            Resultado = cnEmpleado.validarDatos(ceEmpleado);
+
+            if (Resultado == false)
             {
-                MessageBox.Show("ID del inventario no puede ser 0");
-            } else if (tbxDescripcion.Text == "")
+                return;
+            }
+
+            if (ceEmpleado.Id == 0)
             {
-                MessageBox.Show("La descripcion no puede esta vacia");
-            } else if (tbxUnidad.Text == "")
-            {
-                MessageBox.Show("Unidad no puede estar vacia");
-            } else if (tbxPrecio.Text == "")
-            {
-                MessageBox.Show("El precio no puede estar vacio");
-            } else if (tbxCantidad.Text == "")
-            {
-                MessageBox.Show("La cantidad no puede estar vacia");
-            } else if (cbxEstatus.Text == "")
-            {
-                MessageBox.Show("El estatus no puede estar vacio");
+                cnEmpleado.crearEmpleado(ceEmpleado);
             }
             else
             {
-                int c = dgvInventario.Rows.Add();
-                dgvInventario.Rows[c].Cells[0].Value = nudInventario.Value;
-                dgvInventario.Rows[c].Cells[1].Value = tbxDescripcion.Text;
-                dgvInventario.Rows[c].Cells[2].Value = tbxUnidad.Text;
-                dgvInventario.Rows[c].Cells[3].Value = tbxPrecio.Text;
-                dgvInventario.Rows[c].Cells[4].Value = tbxCantidad.Text;
-                dgvInventario.Rows[c].Cells[5].Value = cbxEstatus.Text;
-
-                //BtnActualizar.Enabled = true;
-                btnEliminar.Enabled = true;
-                btnLimpiar.Enabled = true;
+                cnEmpleado.editarEmpleado(ceEmpleado);
             }
 
-            nudInventario.Value = 0;
-            tbxDescripcion.Text = "";
-            tbxUnidad.Text = "";
-            tbxUnidad.Text = "";
-            tbxPrecio.Text = "";
-            tbxCantidad.Text = "";
-            cbxEstatus.Text = "";
+
+            btnNuevo.Enabled = true;
+            btnEditar.Enabled = false;
+            cargarDatos();
+            limpiarForm();
+            tabControl1.TabPages.Remove(tabPage1);
+            tabControl1.TabPages.Add(tabPage2);
+        }
+
+        
+
+        private void btnCancelar_Click_1(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabPage1);
+            tabControl1.TabPages.Add(tabPage2);
+        }
+
+        private void btnLimpiar_Click_2(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que desea eliminar todos los datos?", "Atencion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                CeEmpleado cE = new CeEmpleado();
+                cnEmpleado.eliminarDatos(cE);
+                cargarDatos();
+                limpiarForm();
+            }
+        }
+
+        
+
+        private void btnNuevo_Click_2(object sender, EventArgs e)
+        {
+            limpiarForm();
+            nudEmpleado.Visible = false;
+            lbEmpleado.Visible = false;
+            cbxEstatus.Visible = false;
+            lbEstatus.Visible = false;
+            btnEliminar.Visible = false;
+            tabControl1.TabPages.Remove(tabPage2);
+            tabControl1.TabPages.Add(tabPage1);
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            cbxEstatus.Visible = true;
+            lbEmpleado.Visible = true;
+            cbxEstatus.Visible = true;
+            lbEstatus.Visible = true;
+            cbxEstatus.Enabled = true;
+            tabControl1.TabPages.Remove(tabPage2);
+            tabControl1.TabPages.Add(tabPage1);
+        }
+
+        private void dgvEmpleado_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            MessageBox.Show("Ya puedes Editar este campo");
+            lbEmpleado.Visible = false;
+            btnNuevo.Enabled = false;
+            btnEditar.Enabled = true;
+
+            nudEmpleado.Value = (uint)dgvEmpleado.CurrentRow.Cells["IdEmpleado"].Value;
+            tbxNombre.Text = dgvEmpleado.CurrentRow.Cells["Nombres"].Value.ToString();
+            tbxApellidos.Text = dgvEmpleado.CurrentRow.Cells["Apellidos"].Value.ToString();
+            tbxSalario.Text = dgvEmpleado.CurrentRow.Cells["Salario"].Value.ToString();
+            cbxTipo.Text = dgvEmpleado.CurrentRow.Cells["Tipo"].Value.ToString();
+            cbxEstatus.Text = dgvEmpleado.CurrentRow.Cells["Estatus"].Value.ToString();
+            pictureBox2.Image = Image.FromStream(Imagen.byteToImg(dgvEmpleado.CurrentRow.Cells["Foto"].Value as byte[]));
+            
+            
+            
+        }
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (tbxPrueba.Text != "")
+            {
+                CeEmpleado cE = new CeEmpleado();
+                cnEmpleado.buscarEm(cE);
+                cargarDatos();
+            }
+            CeEmpleado ceEmpleado = new CeEmpleado();
+            ceEmpleado.Busqueda = tbxPrueba.Text;
+
+            MessageBox.Show("" + ceEmpleado.Busqueda);
+        }
+        private void btnEliminar_Click_3(object sender, EventArgs e)
+        {
+            if (nudEmpleado.Value == 0) return;
+
+            if (MessageBox.Show("¿Seguro que deseas eliminar el registro?", "Atencion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                CeEmpleado cE = new CeEmpleado();
+                cnEmpleado.eliminarEmpleado(cE);
+                cargarDatos();
+                limpiarForm();
+            }
+
+            tabControl1.TabPages.Remove(tabPage1);
+            tabControl1.TabPages.Add(tabPage2);
+        }
+
+        //Metodos sin usar
+        
+        
+        private void lbImagen_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+        
+        private void tabPage2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdSeleccionar = new OpenFileDialog();
+
+            ofdSeleccionar.Filter = "Imagenes | jpg; *.png;";
+            ofdSeleccionar.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            ofdSeleccionar.Title = "Seleccionar Imagen";
+
+            if (ofdSeleccionar.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox2.Image = Image.FromFile(ofdSeleccionar.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex.ToString);
+                    throw;
+                }
+                
+            }
+
+
+        }
+
+
+
+        private void EmpleadosVista_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
